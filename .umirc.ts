0237b361211385c36c8e57f69079d272c03fc943
food-billboard <50391608+food-billboard@users.noreply.github.com>
2022-06-24 11:40:14 +0800

    feat(): :tada: init registry

---
+++ .umirc.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from '@umijs/max';
+import router from './config/router'
+import proxy from './config/proxy';
+
+const { REACT_APP_ENV } = process.env;
+
+export default defineConfig({
+  antd: {
+    // configProvider
+    configProvider: {},
+    // themes
+    dark: true,
+    compact: true,
+    // babel-plugin-import
+    import: true,
+    // less or css, default less
+    style: 'less',
+  },
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {
+    title: '@umijs/max',
+  },
+  routes: router,
+  npmClient: 'pnpm',
+  proxy: (proxy as any)[REACT_APP_ENV || 'prod'],
+});
+
